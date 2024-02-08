41f48d563f23fbe56f5da3052def84c1f8607d82
Justin Chun <chunny0501@gmail.com>
2024-02-07 22:53:20 -0500

    Experience stuff

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,7 +3,10 @@ import './resume.css'
 
 const Resume = () => {
   return (
-    <section id='resume'><iframe src="https://drive.google.com/file/d/1BZ5_67a5pYOBV1aASDf_1uH6Tj4wZOM0/preview" allow="autoplay"/></section>
+    <section id='resume'>
+      <h2>My Resume</h2>
+      <iframe src="https://drive.google.com/file/d/1BZ5_67a5pYOBV1aASDf_1uH6Tj4wZOM0/preview" title="resume" allow="autoplay"/>
+    </section>
   )
 }
 
